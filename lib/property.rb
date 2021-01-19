dadc7d2dbc8a8aa8519ac83f39923174193024c3
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-19 14:57:16 +0000

    Sort properties by owner

--- lib/property.rb
+++ lib/property.rb
@@ -3,7 +3,11 @@ require_relative "database_connection"
 class Property
 
     def self.list_properties
-        DatabaseConnection.query("SELECT * FROM properties")
+        DatabaseConnection.query("SELECT * FROM properties;")
+    end
+
+    def self.list_properties_by_owner(user_id)
+        DatabaseConnection.query("SELECT * FROM properties WHERE owned_by_id = '#{user_id}';")
     end
 
 end
--- spec/units/property_spec.rb
+++ spec/units/property_spec.rb
@@ -1,9 +1,15 @@
 require "property"
+require "database_helper"
 
 describe Property do
 
   it "get all properties" do
     expect(Property.list_properties.ntuples).to eq 0
+  end
+
+  it "sort by owner" do
+    2.times{ add_property }
+    expect(Property.list_properties_by_owner(1).ntuples).to eq 2
   end
 
 end
