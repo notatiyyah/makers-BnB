eb95f543c331adb67711e8163d3e74882228dd1e
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-19 14:27:21 +0000

    Add list_properties function

--- lib/property.rb
+++ lib/property.rb
@@ -1,2 +1,9 @@
+require_relative "database_connection"
+
 class Property
+
+    def self.list_properties
+        DatabaseConnection.query("SELECT * FROM properties")
+    end
+
 end
--- spec/units/property_spec.rb
+++ spec/units/property_spec.rb
@@ -2,4 +2,8 @@ require "property"
 
 describe Property do
 
+  it "get all properties" do
+    expect(Property.list_properties.ntuples).to eq 0
+  end
+
 end
