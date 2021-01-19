9f6b0a5b430aac8e5ce3a39fe549f0ffbb588289
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-19 14:53:31 +0000

    Update database_helper to include methods to add test user and property

--- spec/database_helper.rb
+++ spec/database_helper.rb
@@ -5,3 +5,17 @@ def set_up_test_env
     DatabaseConnection.query("TRUNCATE TABLE #{table_name} CASCADE")
   end
 end
+
+def add_user
+  query_string = "INSERT INTO users (user_id, username, password)
+  VALUES( '1', 'test_user', 'test_password' ) "
+  DatabaseConnection.query(query_string)
+end
+
+def add_property
+  num_of_users = DatabaseConnection.query("SELECT COUNT(*) FROM users;").getvalue(0,0).to_i
+  add_user if num_of_users == 0
+  query_string = "INSERT INTO properties (name, owned_by_id, is_available ) 
+  VALUES( 'test_property', '1', 'true' ) "
+  DatabaseConnection.query(query_string)
+end
