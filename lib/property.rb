2d33f8017e16ff46c70655363ed0c3bc9d60369d
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-21 15:01:11 +0000

    Remove references to availability (backend only)

--- lib/property.rb
+++ lib/property.rb
@@ -18,29 +18,20 @@ class Property
         DatabaseConnection.query("SELECT * FROM properties WHERE owned_by_id = '#{user_id}';").map(&@@to_obj)
     end
 
-    def self.list_by_availability(is_available)
-        DatabaseConnection.query("SELECT * FROM properties WHERE is_available = #{is_available};").map(&@@to_obj)
-    end
-
-    def self.set_availability(property_id, is_available)
-        DatabaseConnection.query("UPDATE properties SET is_available = #{is_available} WHERE property_id = '#{property_id}';")
-    end
-
     def self.add(property)
         if property.property_id.nil?
-            DatabaseConnection.query("INSERT INTO properties (name, owned_by_id, is_available ) 
-            VALUES( '#{property.name}', '#{property.owner_id}', '#{property.is_available}' );")
+            DatabaseConnection.query("INSERT INTO properties (name, owned_by_id) 
+            VALUES( '#{property.name}', '#{property.owner_id}');")
         else
-            DatabaseConnection.query("INSERT INTO properties (property_id, name, owned_by_id, is_available ) 
-            VALUES( '#{property.property_id}', '#{property.name}', '#{property.owner_id}', '#{property.is_available}' );") 
+            DatabaseConnection.query("INSERT INTO properties (property_id, name, owned_by_id) 
+            VALUES( '#{property.property_id}', '#{property.name}', '#{property.owner_id}');") 
         end
     end
 
     def self.update(property_id, new_property)
         DatabaseConnection.query("UPDATE properties 
         SET name = '#{new_property.name}',
-        owned_by_id = '#{new_property.owner_id}',
-        is_available = '#{new_property.is_available}'
+        owned_by_id = '#{new_property.owner_id}'
         WHERE property_id = '#{property_id}';")
     end
 
@@ -51,13 +42,11 @@ class Property
     # ^ Class Methods 
     # v Instance methods
 
-    attr_reader :property_id, :name, :owner_id, :is_available
-
+    attr_reader :property_id, :name, :owner_id
     def initialize(info)
         @name = info["name"]
         @owner_id = info["owned_by_id"]
         @property_id = info["property_id"]
-        @is_available = (info["is_available"] == false ? false : true)
         Property.add(self) if info["add_to_db?"].nil? || info["add_to_db?"] == true
     end
 end
--- spec/units/property_spec.rb
+++ spec/units/property_spec.rb
@@ -12,10 +12,6 @@ describe Property do
       expect(property.name).to eq name
       expect(property.owner_id).to eq 1
     end
-
-    it "sets is_available to true as default" do
-      expect(property.is_available).to eq true
-    end
     
     it "adds self to database when created" do
       all_property_names = Property.list.map(&:name)
@@ -37,18 +33,6 @@ describe Property do
 
     it "sort by owner" do
       expect(Property.list_by_owner(1).length).to eq 3
-    end
-
-    it "sort by availability" do
-      expect(Property.list_by_availability(true).length).to eq 2
-      expect(Property.list_by_availability(false).length).to eq 1
-    end
-
-    it "change availability" do
-      available_property_id = DatabaseConnection.query("SELECT property_id FROM properties WHERE is_available = true LIMIT 1;").getvalue(0,0)
-      Property.set_availability(available_property_id, false)
-      expect(Property.list_by_availability(true).length).to eq 1
-      expect(Property.list_by_availability(false).length).to eq 2
     end
 
     it "add property" do
