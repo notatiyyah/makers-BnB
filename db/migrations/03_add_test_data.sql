4c4d840b4ab0d224ce96ac556474d38fbfc71f36
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-22 12:19:18 +0000

    Add description and price fields to properties

--- db/migrations/03_add_test_data.sql
+++ db/migrations/03_add_test_data.sql
@@ -2,11 +2,11 @@ INSERT INTO users_base
 VALUES(1,'test_user', 'test_password', 'test_first_name', 'test_second_name');
 
 INSERT INTO properties_base
-VALUES(1,'booked_property', 1);
+VALUES(1,'booked_property', 1, 'this is a testing property that has been booked', 50.00);
 INSERT INTO properties_base
-VALUES(2,'unbooked_property', 1);
+VALUES(2,'unbooked_property', 1, 'this is a testing property that has been booked', 70.00);
 INSERT INTO properties_base
-VALUES(3,'unbooked_property', 1);
+VALUES(3,'unbooked_property', 1, 'this is a testing property that hasn't been booked', 20.00);
 
 INSERT INTO bookings_base
 VALUES(1, 1, 1, '2021-05-03', '2021-05-07');
